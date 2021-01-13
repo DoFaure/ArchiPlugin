7420428f94fc2b3d3f4fcc8a43f9a669ca245d53
jeremi85e <jeremiesemelin@gmail.com>
2021-01-13 16:57:39 +0100

    Affichage prix + gain

--- src/plugins/factories/ConsumableFactory.java
+++ src/plugins/factories/ConsumableFactory.java
@@ -12,12 +12,12 @@ public class ConsumableFactory implements IConsumableFactory {
 	public List<Consumables> createFarmerConsumables() {
 		List<Consumables> consumablesFarmer = new ArrayList<Consumables>();
 		// Prix, duree, augmentation, nom
-		consumablesFarmer.add(new Consumables(1, 10, 10, "Test"));
-		consumablesFarmer.add(new Consumables(25, 100, 2, "Bouteille d'eau"));
-		consumablesFarmer.add(new Consumables(2, 100, 3, "Coca-Cola"));
-		consumablesFarmer.add(new Consumables(100, 100, 5, "Engrais"));
-		consumablesFarmer.add(new Consumables(500, 100, 25, "Glyphosate"));
-		consumablesFarmer.add(new Consumables(1000, 100, 100, "SuperFarmer"));
+		consumablesFarmer.add(new Consumables(2, 2, 2, "Bouteille d'eau"));
+		consumablesFarmer.add(new Consumables(5, 3, 3, "Coca-Cola"));
+		consumablesFarmer.add(new Consumables(10, 5, 5, "P'tit LU"));
+		consumablesFarmer.add(new Consumables(100, 8, 20, "Engrais"));
+		consumablesFarmer.add(new Consumables(500, 10, 100, "Glyphosate"));
+		consumablesFarmer.add(new Consumables(1000, 5, 500, "SuperFarmer"));
 		return consumablesFarmer;
 	}
 
