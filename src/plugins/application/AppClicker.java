eaabd25401d4c2d6c43884ec7fe62201016d4c62
ArthurDbr <arthur.debar@yahoo.fr>
2021-02-01 17:58:06 +0100

    refacto

--- src/plugins/application/AppClicker.java
+++ src/plugins/application/AppClicker.java
@@ -12,10 +12,6 @@ import appli.interfaces.IFarmerClickerDisplay;
 
 public class AppClicker implements IAppClicker {
 
-	JFrame menuFrame = new JFrame(); // Window
-	JPanel panel = new JPanel(); // Panel contains components (buttons, text boxes, ...)
-	JButton buttonFarmerClicker = new JButton("Farmer Clicker"); // First button that launch Farmer clicker onClick
-	JButton buttonBrewerClicker = new JButton("Brewer Clicker"); // Second button that launch Brewer clicker onClick
 	IFarmerClickerDisplay farmerDisplay;
 
 	/**
@@ -23,34 +19,6 @@ public class AppClicker implements IAppClicker {
 	 */
 	public AppClicker(IFarmerClickerDisplay farmerDisplay) {
 		this.farmerDisplay = farmerDisplay;
-	}
-
-	/**
-	 * @return the menuFrame
-	 */
-	public JFrame getMenuFrame() {
-		return menuFrame;
-	}
-
-	/**
-	 * @param menuFrame the menuFrame to set
-	 */
-	public void setMenuFrame(JFrame menuFrame) {
-		this.menuFrame = menuFrame;
-	}
-
-	/**
-	 * @return the panel
-	 */
-	public JPanel getPanel() {
-		return panel;
-	}
-
-	/**
-	 * @param panel the panel to set
-	 */
-	public void setPanel(JPanel panel) {
-		this.panel = panel;
 	}
 
 }
